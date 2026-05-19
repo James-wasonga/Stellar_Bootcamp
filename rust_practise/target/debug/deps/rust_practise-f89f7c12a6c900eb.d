18cb9504bf7e800bcc9d6856095ace748e97d0d7
James-wasonga <jameswasonga22@gmail.com>
2026-05-19 23:59:39 +0300

    rust basic practise

--- rust_practise/target/debug/deps/rust_practise-f89f7c12a6c900eb.d
+++
@@ -1,6 +0,0 @@
-/mnt/f/Stellar_Bootcamp/rust_practise/target/debug/deps/rust_practise-f89f7c12a6c900eb.d: src/main.rs src/vector.rs
-
-/mnt/f/Stellar_Bootcamp/rust_practise/target/debug/deps/rust_practise-f89f7c12a6c900eb: src/main.rs src/vector.rs
-
-src/main.rs:
-src/vector.rs:
--- rust_practise/target/debug/rust_practise.d
+++
@@ -1,1 +0,0 @@
-/mnt/f/Stellar_Bootcamp/rust_practise/target/debug/rust_practise: /mnt/f/Stellar_Bootcamp/rust_practise/src/main.rs /mnt/f/Stellar_Bootcamp/rust_practise/src/vector.rs
